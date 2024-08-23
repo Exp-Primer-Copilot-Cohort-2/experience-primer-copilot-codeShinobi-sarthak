1c1b364643f95a805692eefa10f38e736ebe0d4f
codeShinobi-sarthak <codeshinobi.sarthak@gmail.com>
2024-08-23 12:08:34 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMembers() {
+    const skills = [
+        { name: 'HTML', level: 5 },
+        { name: 'CSS', level: 4 },
+        { name: 'JavaScript', level: 3 },
+    ];
+    return skills;
+}
